34f26d9b1412fc5f78d065f09f1f1a72cdef551f
Youssef Ramy <youssef.rfarid@gmail.com>
2024-04-28 23:43:23 +0300

    work in progress login page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,7 +1,7 @@
 import { Counter } from '@/components/counter'
 import { Navbar } from '@/components/navbar'
 import DefaultLayout from '@/layouts/default'
- import {Button, Input} from "@nextui-org/react";
+ import {Button, Card, CardHeader, Divider, Input} from "@nextui-org/react";
 import { useMemo, useState } from 'react';
 
 export default function Page() {
@@ -15,8 +15,17 @@ export default function Page() {
     return validateEmail(value) ? false : true;
   }, [value]);
 
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
   return (
-    <div className='flex flex-col min-h-screen justify-center items-center gap-4'>
+    <div className='flex flex-col min-h-screen min-w-screen justify-center items-center'>
+      <Card className='flex flex-col justify-center items-center gap-4 p-6'>
+        <CardHeader className='flex justify-center items-center'>
+            <label className='text-xl font-bold'>Admin Login</label>
+        </CardHeader>
+        <Divider />
     <Input
       value={value}
       type="email"
@@ -26,25 +35,29 @@ export default function Page() {
       color={isInvalid ? "danger" : "success"}
       errorMessage={isInvalid && "Please enter a valid email"}
       onValueChange={setValue}
-      className="max-w-xs"
+      className="w-80"
       isRequired
     />
     <Input
-      value={value}
-      type="email"
-      label="ASHdaddasdad"
+      label="Password"
       variant="bordered"
-      isInvalid={isInvalid}
-      color={isInvalid ? "danger" : "success"}
-      errorMessage={isInvalid && "Please enter a valid email"}
-      onValueChange={setValue}
-      className="max-w-xs"
-      isRequired
+      placeholder="Enter your password"
+      endContent={
+        <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+          {/* {isVisible ? (
+            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          ) : (
+            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          )} */}
+        </button>
+      }
+      type={isVisible ? "text" : "password"}
+      className="w-80"
     />
-    <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
+    <Button isDisabled={isInvalid} radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
       Log in
     </Button>
-
+    </Card>
     </div>
   );
 }
