e1c2d1366ba08a00810b1c0fa09a5fd0d87b1d18
Abdelrahman Zidan <abdelrahman.azdn>
2024-05-02 22:18:53 +0300

    Nothing

--- pages/admin-review.tsx
+++ pages/admin-review.tsx
@@ -1,12 +1,19 @@
+import DefaultLayout from "@/layouts/default";
 import { Button, Card, Navbar, button } from "@nextui-org/react";
+import NextLink from "next/link";
+import { siteConfig } from "@/config/site";
+import { useState } from "react";
 
 export default function adminReviewPage() {
+  const [events, setEvents] = useState([]);
+  const { pressProps, isPressed } = usePress({});
+
   return (
-    <div>
-      <Card>
-        test
-        <Button>test</Button>
-      </Card>
-    </div>
+    <DefaultLayout>
+      <Button>test</Button>
+    </DefaultLayout>
   );
+}
+function usePress(arg0: {}): { pressProps: any; isPressed: any } {
+  throw new Error("Function not implemented.");
 }
