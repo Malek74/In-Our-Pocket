082db2203f14f6ed3fb55a10c1b662bd8fac1064
GasserAli <gasser.ali.emara@gmail.com>
2024-05-04 21:38:01 +0300

    missing few tasks

---
+++ components/openInNewTabIcon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const OpenInNewTabIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" width="1em" height="1em" viewBox="0 0 32 32" id="icon" {...props}>
+      <defs>
+        <style>
+          {`.cls-1 {
+            fill: none;
+          }`}
+        </style>
+      </defs>
+      <g>
+        <path d="M26,26H6V6H16V4H6A2.002,2.002,0,0,0,4,6V26a2.002,2.002,0,0,0,2,2H26a2.002,2.002,0,0,0,2-2V16H26Z"/>
+        <path d="M26,26H6V6H16V4H6A2.002,2.002,0,0,0,4,6V26a2.002,2.002,0,0,0,2,2H26a2.002,2.002,0,0,0,2-2V16H26Z"/>
+      </g>
+      <polygon points="26 6 26 2 24 2 24 6 20 6 20 8 24 8 24 12 26 12 26 8 30 8 30 6 26 6"/>
+      <rect id="_Transparent_Rectangle_" data-name="<Transparent Rectangle>" className="cls-1" width="32" height="32"/>
+    </svg>
+  );
+};
+
+export default OpenInNewTabIcon;
