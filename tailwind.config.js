0cd5958545c0fe4c1f4dcfa7cf8156979518bf32
Malek74 <65236391+Malek74@users.noreply.github.com>
2024-05-04 21:45:49 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,13 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
-    colors: {
-      'blue': '#1fb6ff',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-      'mint': '#ecfdf1',
-      'rose': '#fee7e6',
-      'yellow': '#f9eac2',
+   extend: {
+      colors: {
+        'rose': "#FEE7E6",
+        'mint': '#ECFDF1',
+        'yellow': '#F9EAC2',
+        'Orange': '#FFD898'
+      }
     },
 
 
