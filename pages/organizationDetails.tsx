47d85190b8db504e5a351d8d1a088eaa0c1d4bf4
GasserAli <gasser.ali.emara@gmail.com>
2024-05-11 19:41:55 +0300

    chore: Update package.json and package-lock.json

--- pages/organizationDetails.tsx
+++ pages/organizationDetails.tsx
@@ -73,7 +73,7 @@ export default function Organisations() {
         <div className="relative flex flex-col h-screen">
             <Navbar></Navbar>
             <div className="flex flex-row flex-1">
-                <div className="flex-initial w-[250px]"><Sidebar></Sidebar></div>
+                <div className="flex-initial w-[250px]"><Sidebar elements={[]}></Sidebar></div>
                 <div className="flex-1 flex flex-col">
                 <div>
                 <div className="flex-1 flex flex-col">
